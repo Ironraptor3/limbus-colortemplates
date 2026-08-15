cba6b42bdd9e79c173ac6c0f376222a4a73349f5
Danny Ribaudo <dannyribaudo@gmail.com>
2026-08-15 15:16:39 -0400

    Try to fix path oddities

--- build_install.sh
+++ build_install.sh
@@ -2,8 +2,7 @@
 
 dir=$(dirname $0);
 output=custom.colortemplate
-vimdata=${VIMDATA:-~/.vim};
-cd "${dir}" && ./bin/createColorTemplate.mjs "$@" > "${output}" && vim "+Colortemplate! ${vimdata}" "+q!" "${output}";
+cd "${dir}" && ./bin/createColorTemplate.mjs "$@" > "${output}" && vim "+Colortemplate! ${HOME}/.vim" "+q!" "${output}";
 if [[ $? -ne 0 ]]; then
   echo "$(cd "${dir}" && ./bin/createColorTemplate.mjs --help)";
 fi
